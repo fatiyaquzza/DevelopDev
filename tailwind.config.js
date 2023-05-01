6110cd3148a5f7a13d93edc2349a98a15a56d103
fatiyaquzza <fatiyaquzzaaa@gmail.com>
2023-05-01 17:05:11 +0700

    feat : add font tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,29 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          '"Inter"',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ]
+    },
   },
+},
   plugins: [
     require('flowbite/plugin')
-]
+],
 
 }
