7c9b23d791db50e3129ac343cd2701502f95a280
fatiyaquzza <fatiyaquzzaaa@gmail.com>
2023-05-03 18:48:45 +0700

    feat : change route

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,36 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+      "./node_modules/flowbite/**/*.js",
     ],
-
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+      extend: {
+        fontFamily: {
+          sans: [
+            '"Inter"',
+            'system-ui',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            '"Noto Sans"',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+            '"Noto Color Emoji"',
+          ]
+      },
     },
-
-    plugins: [require('@tailwindcss/forms')],
-};
+  },
+    plugins: [
+      require('flowbite/plugin')
+  ],
+  
+  }
+  
