165a8af1e7ee869c8f1cb19044ea2afd0d69bf04
fatiyaquzza <fatiyaquzzaaa@gmail.com>
2023-05-04 15:13:42 +0700

    feat : change style isi

--- app/Http/Controllers/PortofolioController.php
+++ app/Http/Controllers/PortofolioController.php
@@ -1,20 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Student;
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class PortofolioController extends Controller
 {
     public function index(Request $request){
 
         if($request->has('search')){
-            $data = Portofolio::where('nama','LIKE','%',.$request->search.'%')->paginate(5);
+            $data = Student::where('nama','LIKE','%',$request->search.'%')->paginate(5);
         }else{
-            $data = Portofolio::paginate(5);
+            $data = Student::paginate(5);
         }
 
-        $data = Portofolio::paginate(5);
+        $data = Student::paginate(5);
         return view('portofolio',compact('data'));
     }
 
