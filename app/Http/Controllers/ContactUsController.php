978974e6a124a935c96bdeccee8009b708ae90d9
Wilda Fahera <wilda.f@mhs.unsyiah.ac.id>
2023-05-17 16:48:44 +0700

    feat : create contact us controller

---
+++ app/Http/Controllers/ContactUsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ContactUs;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class ContactUsController extends Controller
+{
+    public function index(){
+        return view('contactUs');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $data = ContactUs::create($request->all());
+        session()->flash("success","Success Message");
+        return redirect('contactUs');
+
+    }
+}
